c85b7c2ab1e2a310bab8f819947ffdd7a65a4009
David Gherlovanu <dgherlovanu098890@gmail.com>
2023-11-09 16:52:54 +0200

    Adding registration to the project

--- components/auth/register-popup.tsx
+++ components/auth/register-popup.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+// UI components
 import {
   Dialog,
   DialogHeader,
@@ -7,9 +10,71 @@ import {
   DialogTitle,
 } from "../ui/dialog";
 
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+// Validation
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const registrationSchema = z.object({
+  username: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+  confirmPassword: z.string().min(6).max(100),
+});
+
+// Supabase client
+import { createClient } from "@/utils/supabase/client";
+
+const supabase = createClient();
 
 export default function RegisterPopup() {
+  // Define the from
+  const form = useForm<z.infer<typeof registrationSchema>>({
+    resolver: zodResolver(registrationSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // Handle form submission
+  const onSubmit = async (fields: z.infer<typeof registrationSchema>) => {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: fields.email,
+        password: fields.password,
+
+        options: {
+          data: {
+            username: fields.username,
+          },
+          emailRedirectTo: `${location.origin}/auth/callback`,
+        },
+      });
+
+      if (error) throw error;
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -22,6 +87,72 @@ export default function RegisterPopup() {
             Register to start sharing your articles!
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-3"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="johndoe12" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="example@gmail.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Register</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
